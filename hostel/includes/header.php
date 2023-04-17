293cd3cee0ac3c373ea0ae1502e77438a6dbadfe
mide6x <adewoleolumide05@gmail.com>
2023-04-17 14:25:00 +0100

    updated header for University of Abuja

--- hostel/includes/header.php
+++ hostel/includes/header.php
@@ -1,11 +1,11 @@
 
 <?php if($_SESSION['id'])
 { ?><div class="brand clearfix">
-		<a href="#" class="logo" style="font-size:16px; color:#fff">ACU HMS</a>
+		<a href="https://uniabuja.edu.ng/" class="logo" style="font-size:16px; color:#fff"><img src="https://arena.mycredentials.ng/wp-content/uploads/2021/06/uniabuja-logo-1-280x300.jpg" class="ts-avatar hidden-side" alt="" /> UNIABUJA HMS</a>
 		<span class="menu-btn"><i class="fa fa-bars"></i></span>
 		<ul class="ts-profile-nav">
 			<li class="ts-account">
-				<a href="#"><img src="img/aculogo.jpeg" class="ts-avatar hidden-side" alt=""> Account <i class="fa fa-angle-down hidden-side"></i></a>
+				<a href="#"><img src="https://th.bing.com/th/id/OIP.PNGQVIsPFaM41E60Zd0EdgHaHx?pid=ImgDet&rs=1" class="ts-avatar hidden-side" alt="" /> Account <i class="fa fa-angle-down hidden-side"></i></a>
 				<ul>
 					<li><a href="my-profile.php">My Account</a></li>
 					<li><a href="logout.php">Logout</a></li>
@@ -17,7 +17,7 @@
 <?php
 } else { ?>
 <div class="brand clearfix">
-		<a href="#" class="logo" style="font-size:16px;">ACU HMS</a>
+		<	<a href="https://uniabuja.edu.ng/" class="logo" style="font-size:16px; color:#fff; text-decoration: none;"><img src="https://arena.mycredentials.ng/wp-content/uploads/2021/06/uniabuja-logo-1-280x300.jpg" class="ts-avatar hidden-side" alt="" /> UNIABUJA HMS</a>
 		<span class="menu-btn"><i class="fa fa-bars"></i></span>
 		
 	</div>
